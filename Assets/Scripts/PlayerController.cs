da197b0912569368d50de0f6db578731c04381c2
Redtama <redtama@me.com>
2019-10-21 18:44:58 +0100

    Created a player object and implemented player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    Vector2 input;
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("PlayerController Initialised");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        movement = input * speed * Time.deltaTime;
+        transform.Translate(movement);
+    }
+}
